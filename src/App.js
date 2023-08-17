6c42a9a7360a7826564fcaaf7d6e0f94f96c7c9e
[SW5] 위민영 <wmy980522@gmail.com>
2023-08-17 09:13:58 +0000

    improve:  <DiaryView></DiaryView> -> <Diary></Diary>

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <CalendarFunc></CalendarFunc>
-      <DiaryView></DiaryView>
+      <Diary></Diary>
       <Nav></Nav>
     </div>
   );
