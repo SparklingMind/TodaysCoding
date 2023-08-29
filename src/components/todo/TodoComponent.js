7be67888a74c178d0bec2e09502cdf842a4b94ba
MinYoungWI <young1y@nate.com>
2023-08-30 01:06:46 +0900

    초기화

--- src/components/todo/TodoComponent.js
+++ src/components/todo/TodoComponent.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { apiInstance } from "../../utils/api";
 import CategoryList from "./CategoryList";
 import CategoryModal from "./CategoryModal";
 import {
@@ -10,54 +11,54 @@ import {
 } from "./TodoStyles";
 
 function TodoComponent() {
+  const [data, setData] = useState([]); // get으로 받아온 데이터(카테고리 조회)
+  const [_id, _setId] = useState([]); //서버에서 온 _id값 저장할 상태
+  const [_name, _setName] = useState([]); //카테고리 이름을 저장할 상태
   const [isCategoryModalOpen, setIsCategoryModalOpen] = useState(false); // 카테고리 모달의 열림/닫힘 상태를 관리
-  const [categories, setCategories] = useState([
-    {
-      // 카테고리와 할 일(todo) 데이터의 초기 상태 설정
-      // 각 카테고리는 이름(name)과 해당 카테고리 내의 할 일 목록(todos)을 포함한다.
-      // 할 일은 text, completed 상태와 원래의 위치를 나타내는 originalIndex를 포함한다.
-      name: "자바스크립트",
-      todos: [
-        { text: "프로미스 이해하기", completed: false, originalIndex: 0 },
-        { text: "비동기 처리 방법 배우기", completed: false, originalIndex: 1 },
-        { text: "ES6+ 문법 익히기", completed: false, originalIndex: 2 },
-      ],
-    },
-    {
-      name: "리액트",
-      todos: [
-        { text: "Hooks 사용법 학습하기", completed: false, originalIndex: 0 },
-        { text: "리액트 라우터 도입하기", completed: false, originalIndex: 1 },
-        {
-          text: "컴포넌트 최적화 방법 알아보기",
-          completed: false,
-          originalIndex: 2,
-        },
-      ],
-    },
-  ]);
+
+  // GET 요청을 보내고 데이터를 받아옴
+  useEffect(() => {
+    //기존에 저장된 이모지 get요청
+    apiInstance
+      .get("/api/users/categories", {})
+      .then((response) => {
+        setData(response.data); // get 데이터를 상태에 저장
+        if (response.data && response.data.length > 0) {
+          _setId(() => {
+            const _idArr = [];
+            for (let i = 0; i < data.length; i++) {
+              _idArr.push(data[i]._id); //서버에서 온 _id값 배열로 저장
+            }
+            return _idArr;
+          });
+          _setName(() => {
+            const nameArr = [];
+            for (let i = 0; i < data.length; i++) {
+              nameArr.push(data[i].name); //서버에서 온 _id값 배열로 저장
+            }
+            return nameArr;
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("데이터를 가져오는 중에 오류가 발생했습니다.:", error);
+      });
+  }, []);
 
   return (
     <TodoContainer style={{ float: "left" }}>
       <CategoryIcon
         src="/CategorySetting.png"
         onClick={() => setIsCategoryModalOpen(true)}
-      />{" "}
-      {/*카테고리 설정 아이콘을 클릭하면 모달이 열린다.*/}
+      />
       {isCategoryModalOpen && (
         <ModalOverlay>
           <ModalContent>
-            <CategoryModal
-              categories={categories}
-              setCategories={setCategories}
-              onClose={() => setIsCategoryModalOpen(false)}
-            />
+            <CategoryModal onClose={() => setIsCategoryModalOpen(false)} />
           </ModalContent>
         </ModalOverlay>
       )}
-      
-      {/* CategoryList 컴포넌트에 카테고리 데이터와 해당 데이터를 수정할 수 있는 함수를 props로 전달 */}
-      <CategoryList categories={categories} setCategories={setCategories} />
+      <CategoryList categories={data} />
     </TodoContainer>
   );
 }
--- src/components/todo/TodoInput.js
+++ src/components/todo/TodoInput.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';  
-import styled from 'styled-components';  
+import React, { useState } from "react";
+import styled from "styled-components";
 
 // 입력 필드와 저장 버튼을 포함하는 컨테이너의 스타일 설정
 const InputContainer = styled.div`
@@ -29,52 +29,13 @@ const SaveButton = styled.button`
   }
 `;
 
-function TodoInput({ category, categories, setCategories, setInputVisible }) {
-  // 현재 입력된 할 일의 상태를 관리하는 state. 초기값은 빈 문자열.
-  const [newTodo, setNewTodo] = useState('');
-
-  // '저장' 버튼을 클릭할 때 실행되는 함수
-  const handleSaveClick = () => {
-    // setCategories를 사용하여 현재 categories의 상태를 업데이트한다.
-    setCategories(prevCategories => {
-      // map 함수를 사용하여 각 카테고리를 순회하며,
-      // 현재 선택된 카테고리와 일치하는 경우 해당 카테고리의 할 일 목록을 업데이트한다.
-      return prevCategories.map(cat => {
-        if (cat.name === category.name) {
-          const updatedTodos = [
-            ...cat.todos, 
-            { 
-              text: newTodo, 
-              completed: false,
-              originalIndex: cat.todos.length  // 현재 todos의 길이를 이용하여 unique한 originalIndex 값을 생성
-            }
-          ];
-          // 일치하는 카테고리의 경우, 업데이트된 할 일 목록으로 카테고리 데이터를 반환
-          return { ...cat, todos: updatedTodos };
-        } else {
-          // 일치하지 않는 카테고리는 그대로 반환
-          return cat;
-        }
-      });
-    });
-    // 할 일을 추가한 후, 입력 필드를 초기화하고 입력 컴포넌트를 숨긴다.
-    setNewTodo('');
-    setInputVisible(false);
-  };
-
-  // 사용자의 입력을 받아오고, '저장' 버튼을 통해 할 일을 저장하는 UI를 반환
+function TodoInput() {
   return (
     <InputContainer>
-      <Input
-        type="text"
-        value={newTodo} // 입력 필드의 값은 newTodo state에 연결
-        onChange={e => setNewTodo(e.target.value)} // 사용자 입력시 newTodo 상태 업데이트
-        placeholder="새로운 할 일을 입력하세요"
-      />
-      <SaveButton onClick={handleSaveClick}>저장</SaveButton>
+      <Input type="text" placeholder="새로운 할 일을 입력하세요" />
+      <SaveButton>저장</SaveButton>
     </InputContainer>
   );
 }
-
 
 export default TodoInput;
