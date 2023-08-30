c7aaa09a02e665a861ae78f1b147f5ea483ed10f
eideun <yed622j@gmail.com>
2023-08-30 21:29:40 +0900

    develop merged

--- src/routes/HOMEPAGE/HomePage.js
+++ src/routes/HOMEPAGE/HomePage.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { useNavigate } from "react-router-dom"
-
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styled from "styled-components"
